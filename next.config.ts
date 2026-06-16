e0635b8d81177ff0670633ce507461aa45ba2ddd
AvatarJay <jayspangnm@gmail.com>
2026-06-16 16:08:26 -0600

    Allow WordPress iframe embed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/avatar",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://lawnmowergrass.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
